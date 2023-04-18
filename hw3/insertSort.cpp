3db2657e88dd60573028be95262b6a4fb6b5438e
鄒雨笙 <yusheng@MacBook-Air.local>
2023-04-18 22:02:00 +0800

    Updated 20230418

--- hw3/insertSort.cpp
+++ hw3/insertSort.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <string>
 #include "Employee.h"
 using namespace std;
 
--- hw3/main.cpp
+++ hw3/main.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <vector>
 #include "Employee.h"
+#include "Attendance.h"
 using namespace std;
 
 // 宣告用來開啟csv檔案的副程式
@@ -18,9 +19,25 @@ void parseCSV(char* &filename, vector<Employee> &employees);
 void insertSort(vector<Employee> &v);
 
 int main(int argc, char *argv[]) {
-    vector<Employee> employees;
+    // 宣告用以儲存原始的csv檔employee資料的vector
+    vector<Employee> employees; 
+    // 宣告用以儲存整理過的employee資料，並以id為物件以Attendance為類別
+    // 的vector．未寫
+    vector<Attendance> attendances; 
+
     parseCSV(argv[1], employees); // 呼叫用來開啟csv檔案的副程式
     insertSort(employees); // 呼叫用來排序vector的副程式
+
+    // 以下還未寫
+
+    // 呼叫用來創建attendances的副程式
+    createAttend(employees, attendances);
+    // 呼叫用來計算連續工作天並找出開始與結束日期的副程式
+    calConWorkday(employees, attendances);
+    selectSort(attendances); // 呼叫用來排序attendances的副程式
+    // 列印出前三名最長連續工作天的員工，每行格式為
+    // 例: 1023, 100, 20230102, 20230310
+    printOut(attendances); 
 
     for (int i = 0; i < employees.size(); ++i) {
         cout << employees[i].getId() << ',' << employees[i].getSignType() << ',' << employees[i].getFtime() << endl;
