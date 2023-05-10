d6a15d5c772155ae918c957dacbc2742eb0dc980
Yu Sheng Tzou <daniel.tzou1021@gmail.com>
2023-05-10 21:31:29 +0800

    Wrote parseText()

--- hw5/parseText.cpp
+++ hw5/parseText.cpp
@@ -1,4 +1,4 @@
-/*  鄒雨笙 F64081070 程式設計二 第4次作業 2023/4/26
+/*  鄒雨笙 F64081070 程式設計二 第5次作業 2023/5/11
 
     此副程式parseCSV()用來讀取csv檔案，並將檔案中的資料存入
     vector<Employee>中
@@ -8,42 +8,57 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <map>
 #include <vector>
 #include <cassert>
-#include "Employee.h"
 using namespace std;
 
-void parseText(char* &filename, vector<Employee> &employees) {
+void parseText(char* &corpus, char* &query, map<int, vector<string> > &c, map<int, vector<string> > &q) { 
     // 宣告一個ifstream型別的物件infile
     ifstream infile;
-    // 開啟當前目錄下名為filename的檔案
-    infile.open(filename);
+    // 開啟當前目錄下名為corpus的檔案
+    infile.open(corpus);
     if (!infile) {
-        cerr << "error: unable to open the input file: " << filename << endl;
+        cerr << "error: unable to open the input file: " << corpus << endl;
     }
 
     string line, field;
     
-    // 將檔案中的資料存入vector<Employee> employees
+    // 將檔案中的資料存入map<int, vector<string> > corpus
     while (getline(infile, line)) {
-        istringstream ss(line); // 將ss綁定至讀取行
-        vector<string> fields;
-        // 將每一行的單字取出
-        while (getline(ss, field, ' ')) { 
-            fields.push_back(field);
+    istringstream ss(line);
+    vector<string> fields;
+    string token;
+    for(char& c : line){
+        if(c == ' ' || c == '"' || c == ','){
+            if (!token.empty()) {
+                fields.push_back(token);
+                token.clear();
+            }
         }
-        int id = stoi(fields[0]);
-        string signType = fields[1];
-        string time = fields[2];
-        // 建立物件emp
-        Employee emp(id, signType, time);
-        employees.push_back(emp);
+        else{
+            token += c;
+        }
     }
-    
+    // Push the last token if it's not empty
+    if (!token.empty()){
+        fields.push_back(token);
+    }
+    int key = stoi(fields[0]);
+    c.insert({key, fields});
+    }
 
     
-    // cout << "Read csv file success" << endl;
+    infile.close();    
+    for (auto it = c.begin(); it != c.end(); ++it) {
+        cout << "Key: " << it->first << ", Value: ";
+        for (const auto& str : it->second) {
+            cout << str << " ";
+        }
+        cout << "\n";
+    }
 
+    cout << "Read txt file success" << endl;
 }
 
 
