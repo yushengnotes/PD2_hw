3a711714bdf45205b365ca5ec632511f0ac33899
Yu Sheng Tzou <daniel.tzou1021@gmail.com>
2023-06-20 21:50:09 +0800

    Update 20230620

--- hw7/calculateRank.cpp
+++ hw7/calculateRank.cpp
@@ -7,6 +7,7 @@
 #include <unordered_map> 
 #include <vector> 
 #include <utility> // for using pairs
+#include <algorithm>
 using namespace std;
 
 void calculateRank(unordered_map <int, map<int, vector< pair<int, pair<double, double> > > > > &storeKeyWord, unordered_map <int, vector< pair<int, double> > > &result) {
@@ -32,6 +33,20 @@ void calculateRank(unordered_map <int, map<int, vector< pair<int, pair<double, d
         result[outer_key_pair.first] = inner_result;
     }
 
+    // For each key-value pair in the map
+    for(auto &pair : result) {
+        // Sort the vector of pairs based on the second element of each pair
+        std::sort(pair.second.begin(), pair.second.end(), 
+            [](const std::pair<int, double>& a, const std::pair<int, double>& b) {
+                if (a.second != b.second)
+                    return a.second > b.second;
+                else
+                    return a.first < b.first;
+            }
+        );
+    }
+
+    // A checkpoint for result
     for(const auto& outer_pair : result){
     cout << "Key: " << outer_pair.first << endl;
     for(const auto& inner_pair : outer_pair.second){
